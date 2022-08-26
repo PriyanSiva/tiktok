a130fb070411e379d15c24bff499676e33b77702
Sivapriyan Sivarasan <sivapriyans@embla.asia>
2022-08-26 10:40:32 +0530

    added schemas

---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'user',
+    title: 'User',
+    typw: 'document',
+    fields: [
+        {
+            name: 'UserName',
+            title: 'User Name',
+            type: 'string'
+        }, 
+        {
+            name: 'image',
+            title:'Image',
+            type: 'string'
+        }
+    ]
+}
